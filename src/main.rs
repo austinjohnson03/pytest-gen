a4cebea92f6b44cebd10857360d01344d56e0e81
austinjohnson03 <amj102998@gmail.com>
2026-02-05 01:30:24 -0500

    basic file i/o. need to add args
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new();
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Failed to open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("Failed to read {}: {}", display, why),
+        Ok(_) => println!("{} contains:\n{}", display, s),
+    }
 }
