9be1d37df697facb69ad46aadfc02c3cd112d5c3
austinjohnson03 <amj102998@gmail.com>
2026-02-05 08:51:09 -0500

    updated file i/o to take cli args
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
-    let path = Path::new();
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("Failed to open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("Failed to read {}: {}", display, why),
-        Ok(_) => println!("{} contains:\n{}", display, s),
+    let args: Vec<String> = env::args().collect();
+    if let Ok(lines) = read_lines(&args[1]) {
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line);
+        }
     }
 }
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
